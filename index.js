44ba07d62647bd0b52216270b7c9ee19c9a35b23
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-12-01 22:39:45 +0200

    Render function

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 import { menuArray } from "./data.js";
 
-console.log(menuArray);
+const mainContainerEl = document.getElementById("main-container");
+const AddBtn = document.getElementById("Add-btn");
+
+// AddBtn.addEventListener("click", function () {
+
+// });
+
+function render() {
+  let ordersFeeds = "";
+
+  menuArray.forEach(function (menuItem) {
+    ordersFeeds += `
+      <div class="order-item" id="${menuItem.id}">
+        <span>${menuItem.emoji}</span>
+        <div class="order-details">
+          <h2>${menuItem.name}</h2>
+          <p>${menuItem.ingredients}</p>
+          <h3>$${menuItem.name}</h3>
+        </div>
+        <button>+</button>
+      </div>
+    `;
+  });
+
+  mainContainerEl.innerHTML = ordersFeeds;
+}
+
+render();
