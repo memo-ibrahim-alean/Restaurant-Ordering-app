2dd4eea9739b95c78a4c59ad092aee13239115a0
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-12-02 10:31:39 +0200

    function selectedItem function

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 import { menuArray } from "./data.js";
 
+let c = 0;
+let total = 0;
 const mainContainerEl = document.getElementById("main-container");
-const AddBtn = document.getElementById("Add-btn");
+const orderReviewEl = document.getElementById("order-review");
 
-// AddBtn.addEventListener("click", function () {
+render();
 
-// });
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.plusbtn) {
+    const AddBtn = document.getElementById(e.target.id);
+
+    selectedItem(e.target.dataset.plusbtn);
+  }
+});
+
+function selectedItem(orderId) {
+  const orderEl = document.getElementById(orderId);
+  const orderTotalEL = document.getElementById("order-total");
+
+  if (c > 0) {
+    orderReviewEl.innerHTML += `
+      <div class="order-review-data">
+        <h3>${orderEl.getElementsByTagName("h2")[0].dataset.name}</h3>
+        <span>remove</span>
+        <h4>${orderEl.getElementsByTagName("h3")[0].dataset.price}</h4>
+      </div>
+    `;
+    c++;
+  } else {
+    c++;
+    orderReviewEl.innerHTML += `
+      <h2>Your order</h2>
+      <div class="order-review-data">
+        <h3>${orderEl.getElementsByTagName("h2")[0].dataset.name}</h3>
+        <span>remove</span>
+        <h4>${orderEl.getElementsByTagName("h3")[0].dataset.price}</h4>
+      </div>
+    `;
+  }
+  orderReviewEl.style.borderBottom = "1px solid #393333";
+
+  total += Number(orderEl.getElementsByTagName("h3")[0].dataset.price);
+
+  orderTotalEL.innerHTML = `
+    <h3>Total price:</h3>
+    <h4>${total}</h4>
+  `;
+}
 
 function render() {
   let ordersFeeds = "";
@@ -13,18 +55,16 @@ function render() {
   menuArray.forEach(function (menuItem) {
     ordersFeeds += `
       <div class="order-item" id="${menuItem.id}">
-        <span>${menuItem.emoji}</span>
+        <span "${menuItem.emoji}">${menuItem.emoji}</span>
         <div class="order-details">
-          <h2>${menuItem.name}</h2>
+          <h2 data-name = ${menuItem.name}>${menuItem.name}</h2>
           <p>${menuItem.ingredients}</p>
-          <h3>$${menuItem.price}</h3>
+          <h3 data-price = "${menuItem.price}">$${menuItem.price}</h3>
         </div>
-        <button>+</button>
+        <button data-plusBtn = "${menuItem.id}"  id="btn-${menuItem.id}">+</button>
       </div>
     `;
   });
 
   mainContainerEl.innerHTML = ordersFeeds;
 }
-
-render();
