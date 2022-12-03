1da1431989f0afa3513f6052e5c63faaa0f69b0b
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2022-12-03 13:25:35 +0200

    remove order

--- index.js
+++ index.js
@@ -1,52 +1,71 @@
 import { menuArray } from "./data.js";
+import { v4 as uuid } from "https://jspm.dev/uuid";
 
 let c = 0;
 let total = 0;
 const mainContainerEl = document.getElementById("main-container");
 const orderReviewEl = document.getElementById("order-review");
+let ordersArr = [];
+let reviewString = "";
 
 render();
 
 document.addEventListener("click", function (e) {
   if (e.target.dataset.plusbtn) {
     const AddBtn = document.getElementById(e.target.id);
-
     selectedItem(e.target.dataset.plusbtn);
+  }
+
+  if (e.target.dataset.remove) {
+    const removeOrderId = e.target.dataset.remove;
+    const removeEL = document.getElementById(`${e.target.dataset.remove}`);
+    let objIndex = ordersArr.findIndex((obj) => obj.newId === removeOrderId);
+    // only splice array when item is found
+    if (objIndex > -1) {
+      ordersArr.splice(objIndex, 1); // 2nd parameter means remove one item only
+      console.log(ordersArr);
+    }
   }
 });
 
+function removeItem(removeEL) {
+  // console.log("removeOrderId: " + removeOrderId);
+}
+
 function selectedItem(orderId) {
-  const orderEl = document.getElementById(orderId);
+  orderReviewEl.innerHTML = "";
+  let orderEl = document.getElementById(orderId);
+  let orderObj = {
+    name: orderEl.getElementsByTagName("h2")[0].dataset.name,
+    price: orderEl.getElementsByTagName("h3")[0].dataset.price,
+    newId: uuid(),
+  };
   const orderTotalEL = document.getElementById("order-total");
 
-  if (c > 0) {
-    orderReviewEl.innerHTML += `
-      <div class="order-review-data">
-        <h3>${orderEl.getElementsByTagName("h2")[0].dataset.name}</h3>
-        <span>remove</span>
-        <h4>${orderEl.getElementsByTagName("h3")[0].dataset.price}</h4>
-      </div>
-    `;
-    c++;
-  } else {
-    c++;
-    orderReviewEl.innerHTML += `
-      <h2>Your order</h2>
-      <div class="order-review-data">
-        <h3>${orderEl.getElementsByTagName("h2")[0].dataset.name}</h3>
-        <span>remove</span>
-        <h4>${orderEl.getElementsByTagName("h3")[0].dataset.price}</h4>
-      </div>
-    `;
-  }
-  orderReviewEl.style.borderBottom = "1px solid #393333";
-
+  ordersArr.push(orderObj);
   total += Number(orderEl.getElementsByTagName("h3")[0].dataset.price);
 
   orderTotalEL.innerHTML = `
-    <h3>Total price:</h3>
-    <h4>${total}</h4>
+  <h3>Total price:</h3>
+  <h4>${total}</h4>
   `;
+  renderOrderArr();
+}
+
+function renderOrderArr() {
+  reviewString = "";
+
+  ordersArr.forEach(function (order) {
+    reviewString += `<div class="order-review-data"  id="${order.newId}">
+        <h3>${order.name}</h3>
+        <span data-remove ="${order.newId}" >remove</span>
+        <h4>${order.price}</h4>
+      </div>`;
+  });
+
+  orderReviewEl.innerHTML = reviewString;
+
+  orderReviewEl.style.borderBottom = "1px solid #393333";
 }
 
 function render() {
